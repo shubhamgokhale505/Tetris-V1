e924c71a3ca8b1fa86dc2531555c0be0ecf1bdf5
Shubham Gokhale <59763482+shubhamgokhale505@users.noreply.github.com>
2024-05-05 17:21:58 +0530

    Update Main.tf

--- Jenkins-terraform/Main.tf
+++ Jenkins-terraform/Main.tf
@@ -63,7 +63,6 @@ resource "aws_instance" "web" {
   instance_type          = "t2.medium"
   key_name               = "microservice"
   vpc_security_group_ids = [aws_security_group.Jenkins-sg.id]
-  user_data              = templatefile("./install_jenkins.sh", {})
   iam_instance_profile   = aws_iam_instance_profile.example_profile.name
 
   tags = {
